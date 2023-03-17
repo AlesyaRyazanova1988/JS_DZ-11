13561ee2958f6aac7d31aa8faf0c61b0f541ae28
Ryazanovsky <ryazanovsky@mail.ru>
2023-03-17 13:15:14 +0300

    git initial

---
+++ script.js
@@ -0,0 +1,37 @@
+const productsData = [{
+    img: "/img/product_article_1.jpg",
+    heading: 'Куртка',
+    textProduct: 'Тонюсенькая куртка на демисезон',
+    price: '10 000 RUB'
+},
+{
+    img: "/img/product_article_2.jpg",
+    heading: 'Костюм',
+    textProduct: 'Необычный женский костюм-двойка со стильным блейзером. Как говорится и в пир и в мир!',
+    price: '20 000 RUB'
+},
+{
+    img: "/img/product_article_3.jpg",
+    heading: 'Худи',
+    textProduct: 'Странное худи в стиле а-ля-бомж',
+    price: '30 000 RUB'
+},
+{
+    img: "/img/product_article_4.jpg",
+    heading: 'Футболка',
+    textProduct: 'Просто футболка. Чем проще - тем дороже',
+    price: '40 000 RUB'
+},
+{
+    img: "/img/product_article_5.jpg",
+    heading: 'Пиджак',
+    textProduct: 'Женский пиджак нежно-голубого оттенка. Лаконично, стильно, дорого',
+    price: '50 000 RUB'
+},
+{
+    img: "/img/product_article_6.jpg",
+    heading: 'Рубашка из льна',
+    textProduct: 'Отличный летний вариант для променада по набережной Майами',
+    price: '60 000 RUB'
+}
+]
